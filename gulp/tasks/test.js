1790f65a99093d8e74b633ad8613e8c2347edb82
Sven Anders Robbestad <robbestad@gmail.com>
2014-12-25 15:23:29 +0100

    updated tests

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -3,17 +3,10 @@ var gulp = require("gulp"),
     shell = require("gulp-shell"),
     concat = require("gulp-concat");
 
-gulp.task("coverage", ["concat:test"], shell.task([
+gulp.task("coverage", shell.task([
     'npm run-script coverage'
 ]));
 
 gulp.task("test",  shell.task([
     'NODE_PATH=./source/core ./node_modules/.bin/mocha test/test.js --reporter nyan'
 ]));
-//
-//gulp.task('test', function () {
-//    return gulp.src('./test/test.js', {read: false})
-//        .pipe(mocha({ui:'bdd',reporter: 'nyan'})
-//    );
-//});
-//
