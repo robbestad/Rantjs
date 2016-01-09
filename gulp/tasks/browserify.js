f963006169c60ddb883733d62c870416c301422a
Sven A Robbestad <robbestad@gmail.com>
2016-01-09 11:29:16 +0100

    add gulp-shell. Original gulpfile was getting too complicated

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,42 +1,9 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var uglify      = require('gulp-uglify');
-var source      = require('vinyl-source-stream'); // makes browserify bundle compatible with gulp
-var rename   = require('gulp-rename');
-var through = require('through2');
-var gutil = require('gulp-util');
-var buffer = require('vinyl-buffer');
-var sourcemaps = require('gulp-sourcemaps');
-var globby = require('globby');
-
-
+var gulp  = require('gulp')
+var shell = require('gulp-shell')
 gulp.task('browserify', function () {
-  var bundledStream = through();
-
-   bundledStream
-   .pipe(source('./source/core/index.js'))
-   .pipe(buffer())
-   .pipe(uglify())
-   .on('error', gutil.log)
-   .pipe(sourcemaps.write('./'))
-   .pipe(rename('rant.min.js'))
-   .pipe(gulp.dest('./dist'));
-
-   globby(['./source/core/*.js'], function(err, entries) {
-     // ensure any errors from globby are handled
-     if (err) {
-       bundledStream.emit('error', err);
-       return;
-     }
-
-     // create the Browserify instance.
-     var b = browserify({
-       entries: entries,
-       debug: true,
-       expose: 'rant'
-     });
-
-     b.bundle().pipe(bundledStream);
-   });
-  return bundledStream;
+   return gulp.src('*.js', {read: false})
+    .pipe(shell([
+      'npm run browserify'
+    ]
+  ))
 });
